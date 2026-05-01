95c3d1255e74792973c8ef765f7fd112c07f757e
thaituyenlang <email.is.not.vaild9@gmail.com>
2026-05-01 09:23:24 +0700

    WHAT

--- dosage/astro.config.mjs
+++ dosage/astro.config.mjs
@@ -9,7 +9,7 @@ export default defineConfig({
 	integrations: [
 		starlight({
 			title: 'the',
-			//social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			social: [{ icon: 'github', label: 'GitHub', href: 'https://www.touchgrasss.com/' }],
 			sidebar: [
 				{
 					label: 'Reference',
